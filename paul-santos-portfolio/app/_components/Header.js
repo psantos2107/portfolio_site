b7aa378bebdf1795ffd4787ed435f83a25dac723
psantos2107 <paul.santos2107@gmail.com>
2024-07-08 00:25:53 -0700

    established routes, chose navy blue bg color

---
+++ paul-santos-portfolio/app/_components/Header.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Header = () => {
+  return <div>This is the Header</div>;
+};
+
+export default Header;
--- paul-santos-portfolio/app/layout.js
+++ paul-santos-portfolio/app/layout.js
@@ -1,17 +1,28 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Paul Portfolio Site",
+  title: {
+    template: "%s - Paul Santos",
+    default: "Paul Portfolio Site",
+  },
   description: "Paul's portfolio site to showcase his skills and work.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className}, box-border m-0 p-0 min-h-screen flex flex-col bg-blue-900`}
+      >
+        <main className="w-full max-w-screen-xl mx-auto border-solid border-black border-2">
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
